abba85d3049a9e1db3b036cc8778a34eb5bfd641
YungCaedo <62652990+YungCaedo@users.noreply.github.com>
2024-05-18 19:34:48 +0200

    optimized command reloading

--- Events/Client/ready.js
+++ Events/Client/ready.js
@@ -7,8 +7,12 @@ module.exports = {
     /**
      * @param { Client } client 
      */
-    
+
     async execute(client) {
-        for(const [key] of client.guilds.cache) await client.guilds.cache.get(key).commands.set(Array.from(client.commands.values()));
+        for(const [key] of client.guilds.cache) {
+            const commands = (await client.guilds.cache.get(key).commands.fetch()).map((command) => command.name);
+            if(JSON.stringify(commands) !== JSON.stringify(Array.from(client.commands.keys())))
+                await client.guilds.cache.get(key).commands.set(Array.from(client.commands.values()));
+        }
     }
 }
