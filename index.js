d6c81b021bed98f74077e2af24457b30c7ac4642
YungCaedo <62652990+YungCaedo@users.noreply.github.com>
2024-05-30 19:00:10 +0200

    improved handler loading

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 require('dotenv').config();
 const { Client, Collection, } = require('discord.js');
+const validation = require('./utils/Validation');
 
 const client = new Client({ partials: [], intents: 131071 });
 client.commands = new Collection();
 client.events = new Collection();
+client.validation = validation;
 
-["Commands", "Events"].forEach(async (handler) => await require(`./Handlers/${handler}`)(client));
+const handlers = fs.readdirSync(path.join(__dirname, 'handlers/').replace(/\\/g,"/"), { recursive: true }).filter((file) => file.endsWith('.js'));
+handlers.forEach(async (handler) => await require(`./handlers/${handler.split('.')[0]}`)(client));
 
 client.login(process.env['TOKEN']).then(() => {
     console.log(
